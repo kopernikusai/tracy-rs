95f56504d262c7a17d29914a893b1f9f5da7f075
Chip Collier <photex@gmail.com>
2020-04-28 15:38:08 +0200

    Prevent things from getting optimized away somehow

--- build.rs
+++ build.rs
@@ -1,13 +1,14 @@
 fn main() {
     let mut cc_build = cc::Build::new();
-    cc_build.use_plt(false)
-            .flag_if_supported("-flto")
-            .flag_if_supported("/LTCG")
-            .cpp(true)
-            .flag_if_supported("-Wno-implicit-fallthrough")
-            .flag_if_supported("-Wno-unused-function")
-            .flag_if_supported("-Wno-unused-parameter")
-            .include("./tracy");
+    cc_build
+        .use_plt(false)
+        // .flag_if_supported("-flto")
+        // .flag_if_supported("/LTCG")
+        .cpp(true)
+        .flag_if_supported("-Wno-implicit-fallthrough")
+        .flag_if_supported("-Wno-unused-function")
+        .flag_if_supported("-Wno-unused-parameter")
+        .include("./tracy");
 
     if cfg!(feature = "dll") {
         cc_build.file("./tracy/TracyClientDLL.cpp");
@@ -27,5 +28,5 @@ fn main() {
         cc_build.define("TRACY_ON_DEMAND", None);
     }
 
-    cc_build.compile("libtracy.a");
+    cc_build.compile("tracy");
 }
